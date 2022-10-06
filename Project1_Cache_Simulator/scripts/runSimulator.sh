d51b9512118f905b347d894da2a4bd4bdb8b4e63
stephencdevaney <stephendevaney@rocketmail.com>
2022-10-06 15:09:49 -0500

    Added places in script file for NWay and Fully
    needs cleared our create and test code in if statements

--- Project1_Cache_Simulator/scripts/runSimulator.sh
+++ Project1_Cache_Simulator/scripts/runSimulator.sh
@@ -1,13 +1,15 @@
 #/bin/bash
-
-
 # Stephen Devaney
 # CS5375 - Computer Systems Organization and Architecture
 # Programming Project 1
 # Cache Simulator
 # 10/04/2022
 
-export RUN_DIRECT_MAPPED=true
+
+export RUN_DIRECT_MAPPED=false
+export RUN_NWAY_ASSOCIATIVE=true
+export RUN_FULLY_ASSOCIATIVE=false
+
 
 # if cachesim.exe does not exist in the current working directory try to compile it
 if ! test -f "./cachesim.exe"; then
@@ -26,6 +28,7 @@ if ! test -f "./cachesim.exe"; then
 	fi
 fi
 
+
 # if the compilation was successful run the cache simulator other wise output error message
 if test -f "./cachesim.exe"; then
 	# Create an output directory if it does not exists
@@ -33,8 +36,41 @@ if test -f "./cachesim.exe"; then
 		mkdir CachesimOutput
 	fi
 
+
 	# Runs a direct mapped cache if RUN_DIRECT_MAPPED is true.
 	if [[ $RUN_DIRECT_MAPPED == true ]] ; then
+		#Test the Direct Mapped Cache for all trace files and place all output into one output file
+		#echo -e "Simulating Direct Mapped Cache on tracefile 'hw5_memoryaddr'" > CachesimOutput/direct_mapped_output.txt
+		#./cachesim.exe direct trace_for_students/hw5_memoryaddr >> CachesimOutput/direct_mapped_output.txt
+		#echo -e "\n\nSimulating Direct Mapped Cache on tracefile 'trace.stream_10'" >> CachesimOutput/direct_mapped_output.txt
+		#./cachesim.exe direct trace_for_students/trace.stream_10 >> CachesimOutput/direct_mapped_output.txt
+		#echo -e "\n\nSimulating Direct Mapped Cache on tracefile 'trace.stream_20'" >> CachesimOutput/direct_mapped_output.txt
+		#./cachesim.exe direct trace_for_students/trace.stream_20 >> CachesimOutput/direct_mapped_output.txt
+		#echo -e "\n\nSimulating Direct Mapped Cache on tracefile 'trace.stream'" >> CachesimOutput/direct_mapped_output.txt
+		#./cachesim.exe direct trace_for_students/trace.stream >> CachesimOutput/direct_mapped_output.txt
+		#echo -e "\n\nSimulating Direct Mapped Cache on tracefile 'trace.hpcg'" >> CachesimOutput/direct_mapped_output.txt
+		#./cachesim.exe direct trace_for_students/trace.hpcg >> CachesimOutput/direct_mapped_output.txt
+	fi
+
+
+	# Runs a N-Way Associative Cache if RUN_NWAY_ASSOCIATIVE is true.
+	if [[ RUN_NWAY_ASSOCIATIVE == true ]] ; then
+		#Test the Direct Mapped Cache for all trace files and place all output into one output file
+		echo -e "Simulating Direct Mapped Cache on tracefile 'hw5_memoryaddr'" > CachesimOutput/direct_mapped_output.txt
+		./cachesim.exe direct trace_for_students/hw5_memoryaddr >> CachesimOutput/direct_mapped_output.txt
+		echo -e "\n\nSimulating Direct Mapped Cache on tracefile 'trace.stream_10'" >> CachesimOutput/direct_mapped_output.txt
+		./cachesim.exe direct trace_for_students/trace.stream_10 >> CachesimOutput/direct_mapped_output.txt
+		echo -e "\n\nSimulating Direct Mapped Cache on tracefile 'trace.stream_20'" >> CachesimOutput/direct_mapped_output.txt
+		./cachesim.exe direct trace_for_students/trace.stream_20 >> CachesimOutput/direct_mapped_output.txt
+		echo -e "\n\nSimulating Direct Mapped Cache on tracefile 'trace.stream'" >> CachesimOutput/direct_mapped_output.txt
+		./cachesim.exe direct trace_for_students/trace.stream >> CachesimOutput/direct_mapped_output.txt
+		echo -e "\n\nSimulating Direct Mapped Cache on tracefile 'trace.hpcg'" >> CachesimOutput/direct_mapped_output.txt
+		./cachesim.exe direct trace_for_students/trace.hpcg >> CachesimOutput/direct_mapped_output.txt
+	fi
+
+
+	# Runs a FULLY Associative Cache if RUN_FULLY_ASSOCIATIVE is true.
+	if [[ RUN_FULLY_ASSOCIATIVE == true ]] ; then
 		#Test the Direct Mapped Cache for all trace files and place all output into one output file
 		echo -e "Simulating Direct Mapped Cache on tracefile 'hw5_memoryaddr'" > CachesimOutput/direct_mapped_output.txt
 		./cachesim.exe direct trace_for_students/hw5_memoryaddr >> CachesimOutput/direct_mapped_output.txt
