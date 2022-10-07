c61f47a4eead101e754558f92dc08fbc901c753d
stephencdevaney <stephendevaney@rocketmail.com>
2022-10-07 18:24:00 -0500

    Update runSimulator.sh

--- Project1_Cache_Simulator/scripts/runSimulator.sh
+++ Project1_Cache_Simulator/scripts/runSimulator.sh
@@ -61,8 +61,8 @@ if test -f "./cachesim.exe"; then
 		for i in ${!way[@]}; do
 			echo -e "Testing all line sizes and cache sizes of a ${!way[i]} way associative cache for all trace files.:" > CachesimOutput/${way[$i]}way_associative_output.txt
 			for j in ${!line_size[@]}; do
-				echo -e "Simulating ${way[$i]} cache with a fixed cache size of 32768 bytes and a line size of ${line_size[$j]} bytes on tracefile hw5_memoryaddr.\n" >> CachesimOutput/${way[$i]}way_associative_output.txt
-				./cachesim.exe ${!way[i]} -b ${line_size[$j]} -c 32768 trace_for_students/hw5_memoryaddr >> CachesimOutput/${way[$i]}way_associative_output.txt
+				echo -e "\nSimulating ${way[$i]} cache with a fixed cache size of 32768 bytes and a line size of ${line_size[$j]} bytes on tracefile hw5_memoryaddr." >> CachesimOutput/${way[$i]}way_associative_output.txt
+				./cachesim.exe ${!way[i]}-way -b ${line_size[$j]} -c 32768 trace_for_students/hw5_memoryaddr >> CachesimOutput/${way[$i]}way_associative_output.txt
 			done
 
 			for k in ${!cache_size[@]}; do
